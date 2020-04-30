d68b8519c4e8b647024d1b97a2fe16e2ebd64c65
ChrisLepko <57542929+ChrisLepko@users.noreply.github.com>
2020-04-30 03:58:20 +0200

    Initial commit - backend

---
+++ backend/GoEshop/src/main/java/com/github/ChrisLepko/GoEshop/GoEshopApplication.java
@@ -0,0 +1,13 @@
+package com.github.ChrisLepko.GoEshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoEshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoEshopApplication.class, args);
+	}
+
+}
---
+++ backend/GoEshop/src/test/java/com/github/ChrisLepko/GoEshop/GoEshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ChrisLepko.GoEshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoEshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
