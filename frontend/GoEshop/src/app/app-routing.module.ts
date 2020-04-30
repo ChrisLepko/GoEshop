1d8e89136272a0f668a50eeb0aec05ffd59976b5
ChrisLepko <57542929+ChrisLepko@users.noreply.github.com>
2020-04-30 23:53:30 +0200

    Login and registration - frontend next update

--- frontend/GoEshop/src/app/app-routing.module.ts
+++ frontend/GoEshop/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LoginComponent},
+  {path: "login", component: LoginComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "products", component: ProductListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/GoEshop/src/app/app.module.ts
+++ frontend/GoEshop/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FooterComponent,
+    RegisterComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
